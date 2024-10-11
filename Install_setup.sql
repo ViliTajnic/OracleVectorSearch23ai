2e8fbe91fae5dbe2a6c70f1c38bd510aa75d2215
Vili Tajnic <vili@lokalno.com>
2024-10-11 20:43:40 +0200

    install sql added
    tablespace and user add

---
+++ Install_setup.sql
@@ -0,0 +1,13 @@
+-- connect as user sys to FREEPDB1
+
+create bigfile tablespace TBS_VECTOR datafile size 256M autoextend on maxsize 2G;
+
+create user vector_user identified by "Oracle_4U"
+default tablespace TBS_VECTOR temporary tablespace TEMP
+quota unlimited on TBS_VECTOR;
+GRANT create mining model TO vector_user;
+
+-- Grant the 23ai new DB_DEVELOPER_ROLE to the user
+grant DB_DEVELOPER_ROLE to vector_user;
+
+
