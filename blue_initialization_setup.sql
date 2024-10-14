da3f5f361ad0ddb0bf3bb6e758b925872a165b6c
Vili Tajnic <vili@lokalno.com>
2024-10-14 10:49:33 +0200

    Update blue_initialization_setup.sql

--- blue_initialization_setup.sql
+++ blue_initialization_setup.sql
@@ -1,25 +1,30 @@
 -- BLUE connect as user sys to FREEPDB1
--- delete the TBS_VECTOR01 datafile befor start
-/*DROP TABLESPACE TBS_VECTOR01
-   INCLUDING CONTENTS AND DATAFILES;
-drop user "VECTOR_USER01" CASCADE;*/
+-- delete the TBS_VECTOR01 datafile and VECTOR_USER01 before start
+/* 
 
+DROP TABLESPACE TBS_VECTOR01
+   INCLUDING CONTENTS AND DATAFILES;
+drop user "VECTOR_USER01" CASCADE; 
+
+*/
+
+-- Step 1 - Create Tablespace
 create bigfile tablespace TBS_VECTOR01 datafile '/opt/oracle/oradata/FREE/FREEPDB1/tbs_vector01.dbf' size 256M autoextend on maxsize 2G;
 
--- delete vector_user01 for demo -- 
-
-
+-- Step 2 - Create User and Grant Accesses
 create user vector_user01 identified by "Oracle_4U"
 default tablespace TBS_VECTOR01 temporary tablespace TEMP
 quota unlimited on TBS_VECTOR01;
 GRANT create mining model TO vector_user01;
 
--- Grant the 23ai new DB_DEVELOPER_ROLE to the user
+-- Step 3 - Grant the 23ai new DB_DEVELOPER_ROLE to the user
 grant DB_DEVELOPER_ROLE to vector_user01;
 
 
-/*getting a data set downloaded here https://objectstorage.eu-frankfurt-1.oraclecloud.com/n/fro8fl9kuqli/b/AIVECTORS/o/dataset_200K.txt.
-The next step will be creating an external table on top of this file*/
+/*
+Getting a data set downloaded here: https://frt4wbawy9o0.objectstorage.eu-frankfurt-1.oci.customer-oci.com/p/jhEbjXLm7R5FjqfOv37lmjoZBQa6bcINYAQMnomiSVwK9Oal5kfIspAObeBrlEQo/n/frt4wbawy9o0/b/work-share/o/VSDemo/dataset_200K.txt
+The next step will be creating an external table on top of this file
+*/
 
 -- connect as user sys to FREEPDB1
 
